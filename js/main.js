63fe31944b570d9105698b401dec72a76e420d74
whiteEuphoria <prognikitos@gmail.com>
2022-03-03 18:17:53 +0200

    Create section offer, bestsellers, promo

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  speed: 800,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  keyboard: {
+    enabled : true
+  },
+  autoplay: {
+    delay: 3000,
+    stopOnLastSlide: false,
+    disableOnInteraction: true
+  }
+});
